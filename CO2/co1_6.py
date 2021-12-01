9f3db85c150dd560a6fff943a79fa32ad7f41037
SREERAG T V <75733117+SreeragTV99@users.noreply.github.com>
2021-12-01 15:21:07 +0530

    Delete co1_6.py

--- CO2/co1_6.py
+++
@@ -1,8 +0,0 @@
-string=str(input("Enter the string : "))
-freq = {}
-for i in string:
-    if i in freq:
-        freq[i] += 1
-    else:
-        freq[i] = 1
-print("Count of all characters : "+ str(freq))
